e80c2a9c965fe8417fe921dddc36a96a03467dab
Daniel Scarlatta <104516255+DanielScarlatta@users.noreply.github.com>
2023-08-07 18:14:31 -0300

    paraticas

---
+++ JS III - DOM/aulas/4. Obtendo elementos HTML do DOM/index.js
@@ -0,0 +1,20 @@
+function show() {
+    const contactlist = document.getElementById("contact-list")
+    console.log(contactlist)
+
+    const listElements = document.getElementsByTagName("li")
+    console.log(listElements)
+
+    const contactImputs = document.getElementsByClassName("contact-input")
+    console.log(contactImputs)
+
+    const contacts = document.querySelectorAll("#contact-list > li > label")
+    console.log(contacts)
+
+    const contact1 = document.getElementsByName("contact1")
+    console.log(contact1)
+
+    const firstContact = document.querySelector("#contact-list > li > label")
+    console.log(firstContact)
+
+}
