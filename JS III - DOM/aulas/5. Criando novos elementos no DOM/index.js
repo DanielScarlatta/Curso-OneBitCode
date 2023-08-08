6c1e9654eb5ad7736ab85c8b675643b50a291310
Daniel Scarlatta <104516255+DanielScarlatta@users.noreply.github.com>
2023-08-08 17:10:56 -0300

    praticas

---
+++ JS III - DOM/aulas/5. Criando novos elementos no DOM/index.js
@@ -0,0 +1,31 @@
+/*function addInput() {
+    const ul = document.getElementById("inputs")
+
+    const newLi = document.createElement("li")
+    newLi.className = "list-item"
+    newLi.innerText = "Novo input: "
+
+    const newInput = document.createElement("input")
+    newInput.type = "text"
+    newInput.name = "input"
+    
+    newLi.appendChild(newInput)
+    ul.appendChild(newLi)
+}*/
+
+function addInput() {
+    const ul = document.getElementById("inputs")
+
+    const newLi = document.createElement("li")
+    const comunicacao = document.getElementById("requery")
+    newLi.name = "input-name"
+    newLi.className = comunicacao.value
+    newLi.innerText = comunicacao.value
+    console.log(newLi)
+    ul.appendChild(newLi)
+
+}
+
+
+
+
