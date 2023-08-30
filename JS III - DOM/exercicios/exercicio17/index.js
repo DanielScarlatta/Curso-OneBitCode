32733f23c6d2bd333c8aef034e93b9286c494c4a
Daniel Scarlatta <104516255+DanielScarlatta@users.noreply.github.com>
2023-08-30 17:59:06 -0300

    praticas

--- JS III - DOM/exercicios/exercicio17/index.js
+++ JS III - DOM/exercicios/exercicio17/index.js
@@ -24,13 +24,46 @@ function createTechnology() {
     inputNameTec.type = "text"
     
     div.append(br, labelNameTec, inputNameTec)
-    
-    const labelRadioTec = document.createElement("label")
-    labelRadioTec.innerText = "Tempo de experiencia: "
 
-    const b
+    const space = document.createElement("br")//faz a quedra da linha
+    const labelRadioTecOne = document.createElement("label")
+    labelRadioTecOne.innerText = "Tempo de experiencia: "
 
+    const inputRadioTecOne = document.createElement("input")
+    inputRadioTecOne.type = "radio"
+    inputRadioTecOne.name = "experienceTec"
+    inputRadioTecOne.className = "experienceTec1"
+    inputRadioTecOne.value = "0-2 anos"
+    const labelEx = document.createElement("label")
+    labelEx.innerText = "0-2 anos"
 
+    div.append(space, labelRadioTecOne, inputRadioTecOne, labelEx)
+
+    const inputRadioTecTwo = document.createElement("input")
+    inputRadioTecTwo.type = "radio"
+    inputRadioTecTwo.name = "experienceTec"
+    inputRadioTecTwo.className = "experienceTec2"
+    inputRadioTecTwo.value = "3-4 anos"
+    const labelEx2 = document.createElement("label")
+    labelEx2.innerText = "3-4 anos"
+
+    div.append(inputRadioTecTwo, labelEx2)
+
+    const inputRadioTecThree = document.createElement("input")
+    inputRadioTecThree.type = "radio"
+    inputRadioTecThree.name = "experienceTec"
+    inputRadioTecThree.className = "experienceTec3"
+    inputRadioTecThree.value = "5+ anos"
+    const labelEx3 = document.createElement("label")
+    labelEx3.innerText = "5+ anos"
+
+    div.append(inputRadioTecThree, labelEx3)
+
+    const buttonRemoveTec = document.createElement("input")
+    buttonRemoveTec.type = "submit"
+
+    div.appendChild(buttonRemoveTec)
+    technology.appendChild(div)
 }
 
 const formAction = document.getElementById("formAction")
