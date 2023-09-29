a02c27ae6b10bc6f36557c5538306dfb2ca0fa4d
Daniel Dias Scarlatta <daniel.scarlatta@capitalconsig.local>
2023-09-29 17:06:37 -0300

    praticas

--- JS III - DOM/exercicios/exercicio17/index.js
+++ JS III - DOM/exercicios/exercicio17/index.js
@@ -1,118 +1,54 @@
-/*
--criar uma pagina que vai ter um formulario com as informações abaixo
-* labelName, inputName >>> vai colocar o nome no programador
-*buttonAddTec >>> vai criar um formulario (div) com as especificações de tecnologia
-*labelNameTec, buttonRadioTec, buttonRemoveTec >>> fomulario(div) que vai ser criado
-*buttonEnviar >>> pega as informações de todas as tecnologias e cria um objeto dentro de um array que ira armazenar as informações do programador apos isso ele limpa o formulario
+const form = document.getElementById("listTechnology")
+const bottonAddTec = document.getElementById("addTechnology")
+let cont = 0
 
--Usar eventos criados apenas por javaScript
+function createTechnologyList(event) {
+    const ul = document.getElementById("tecList")
+    const li = document.createElement("li")
+    li.id = "tecLi-" + cont
 
-labelName, inputName : buttonAddTec : buttonEnviar>
-labelNameTec, buttonRadioTec, buttonRemoveTec
+    const labelName = document.createElement("label")
+    labelName.innerText = "Tecnologia: "
 
-*/
-const arry = []
-const technology = document.getElementById("technology")
-const formAction = document.getElementById("formAction")
-let indice = 0
+    const nameTec = document.createElement("input")
+    nameTec.type = "text"
+    nameTec.name = "nameTec"
+    nameTec.id = "tecName-" + cont
 
-function createTechnology() {
-    const div = document.createElement("div")
-    
-    const br = document.createElement("br")
-    const labelNameTec = document.createElement("label")
-    labelNameTec.innerText = "Nome da tecnologia: "
+    const space = document.createElement("br")
 
-    const inputNameTec = document.createElement("input")
-    inputNameTec.type = "text"
-    inputNameTec.id = "inputNameTec" + indice
-    
-    div.append(br, labelNameTec, inputNameTec)
+    const labelTime = document.createElement("label")
+    labelTime.innerText = "Tempo de experiencia: "
 
-    const space = document.createElement("br")//faz a quedra da linha
-    const labelRadioTecOne = document.createElement("label")
-    labelRadioTecOne.innerText = "Tempo de experiencia: "
+    const timeExperience1 = document.createElement("input")
+    timeExperience1.type = "radio"
+    timeExperience1.name = "radioExperienceTime"
+    timeExperience1.value = "0-2 anos"
 
-    const inputRadioTecOne = document.createElement("input")
-    inputRadioTecOne.type = "radio"
-    inputRadioTecOne.name = "experienceTec" + indice
-    inputRadioTecOne.className = "experienceTec" + indice
-    inputRadioTecOne.value = "0-2 anos"
-    const labelEx = document.createElement("label")
-    labelEx.innerText = "0-2 anos"
+    const labelTimeExperience1 = document.createElement("label")
+    labelTimeExperience1.innerText = "0-2 anos"
 
-    div.append(space, labelRadioTecOne, inputRadioTecOne, labelEx)
+    const timeExperience2 = document.createElement("input")
+    timeExperience2.type = "radio"
+    timeExperience2.name = "radioExperienceTime"
+    timeExperience2.value = "3-4 anos"
 
-    const inputRadioTecTwo = document.createElement("input")
-    inputRadioTecTwo.type = "radio"
-    inputRadioTecTwo.name = "experienceTec" + indice
-    inputRadioTecTwo.className = "experienceTec" + indice
-    inputRadioTecTwo.value = "3-4 anos"
-    const labelEx2 = document.createElement("label")
-    labelEx2.innerText = "3-4 anos"
+    const labelTimeExperience2 = document.createElement("label")
+    labelTimeExperience2.innerText = "3-4 anos"
 
-    div.append(inputRadioTecTwo, labelEx2)
+    const timeExperience3 = document.createElement("input")
+    timeExperience3.type = "radio"
+    timeExperience3.name = "radioExperienceTime"
+    timeExperience3.value = "5+ anos"
 
-    const inputRadioTecThree = document.createElement("input")
-    inputRadioTecThree.type = "radio"
-    inputRadioTecThree.name = "experienceTec" + indice
-    inputRadioTecThree.className = "experienceTec" + indice
-    inputRadioTecThree.value = "5+ anos"
-    const labelEx3 = document.createElement("label")
-    labelEx3.innerText = "5+ anos"
+    const labelTimeExperience3 = document.createElement("label")
+    labelTimeExperience3.innerText = "5+ anos"
 
-    div.append(inputRadioTecThree, labelEx3)
+    li.append(labelName, nameTec, space, labelTime, timeExperience1, labelTimeExperience1, timeExperience2, labelTimeExperience2 ,timeExperience3, labelTimeExperience3)
+    ul.appendChild(li)
 
-    const space4 = document.createElement("br")
-    const buttonRemoveTec = document.createElement("input")
-    buttonRemoveTec.type = "submit"
-    buttonRemoveTec.value = "Excluir tecnologia"
-
-    indice++
-
-    div.append(space4 ,buttonRemoveTec)
-    technology.appendChild(div)
-}
-
-technology.addEventListener("submit", function (ev) {
-    ev.preventDefault()
-    const buttonTecListener = ev.submitter.closest("div")
-    technology.removeChild(buttonTecListener)
-
-})
-
-
-function sendToArry() {
-    const tecListAdd = []
-    const name = document.getElementById("fullname").value
-    const readTec = document.querySelectorAll("div")
-    let indice = 0
-
-    readTec.forEach(function (ev) {
-        const nameTec = ev.children
-        const nameTecInput = nameTec[2].value
-        const radioInput = document.querySelector(".experienceTec" + indice + ':checked').value
-        
-        tecListAdd.push({nameTec: nameTecInput, exTemp: radioInput})
-        indice++
-    })
-    arry.push({fullname: name, tecList: tecListAdd})
-    console.log(arry)
+    cont++
 }
 
 
-formAction.addEventListener("submit", function (ev) {
-    ev.preventDefault()
-
-    if(ev.submitter.id == "buttonAddTec") {
-        //criar tec
-        createTechnology()
-
-    } else {
-        //envia o form para o arry 
-        sendToArry()
-    }
-})
-
-
-
+bottonAddTec.addEventListener("click", createTechnologyList)
