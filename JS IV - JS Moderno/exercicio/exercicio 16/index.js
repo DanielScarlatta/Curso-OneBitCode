6c4a5bdf17abe190058f41f034c55cf7e5dc7ea4
Daniel Scarlatta <104516255+DanielScarlatta@users.noreply.github.com>
2024-01-10 17:22:15 -0300

    praticas

--- JS IV - JS Moderno/exercicio/exercicio 16/index.js
+++ JS IV - JS Moderno/exercicio/exercicio 16/index.js
@@ -1,8 +1,17 @@
 const dayjs = require('dayjs')
 
-function DataProperty(Birthday) {
-  dayjs().format('DD/MM/YYYY')
-  console.log(dayjs(Birthday))
+function DataProperty(birthday) {
+  const birthdayFormat = dayjs(birthday).format('MM/DD/YYYY')
+  
+  const age = dayjs().diff(birthdayFormat, 'y')
+  const nextBirthday = dayjs(birthdayFormat).add(age+1, 'y').format('DD/MM/YYYY')
+  const daysOfBrithday = dayjs(dayjs(nextBirthday).format('DD/MM/YYYY')).diff(dayjs(), 'd')
+
+  console.log(`Idade atual: ${age} anos`)
+  console.log(`O proximo aniversario é ${nextBirthday}`)
+  console.log(`Falta ${daysOfBrithday} para seu aniversario`)
 }
 
-DataProperty('05/03/2023')
+// MM/DD/YYYY  >> DD/MM/YYYY
+DataProperty('03/05/2003')
+
