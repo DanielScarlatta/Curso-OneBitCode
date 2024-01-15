00c475d929afd3aff0aff85833a285b3e2de1fd6
Daniel Scarlatta <104516255+DanielScarlatta@users.noreply.github.com>
2024-01-15 20:01:41 -0300

    praticas

--- JS IV - JS Moderno/exercicio/exercicio 17/webpack.config.js
+++ JS IV - JS Moderno/exercicio/exercicio 17/webpack.config.js
@@ -7,8 +7,8 @@ module.exports = {
   mode: 'production',
   module: {
     rules: [
-      {test: /\.js$/, use: 'babel-loader'},
-      {test: /\.css$/, use: ['style-loader', 'css-loader']}
+      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {test: /\.js$/, use: ['babel-loader']}
     ]
   },
   output: {
